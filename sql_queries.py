6adf0539058e7494312d88a1353eca92ee7b821e
magusd <vitor.lobs@gmail.com>
2020-05-31 03:07:42 -0300

    half way through the inserts

--- sql_queries.py
+++ sql_queries.py
@@ -1,50 +1,59 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS plays"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS timeseries"
+time_table_drop = "DROP TABLE IF EXISTS times"
 
 # CREATE TABLES
 
 # songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
 songplay_table_create = ("""
+CREATE TABLE songplays (songplay_id int, start_time timestamp, user_id int, level int, song_id varchar (255), artist_id varchar (255), session_id varchar(255), location varchar(255), user_agent varchar(255));
 """)
 
 # user_id, first_name, last_name, gender, level
 user_table_create = ("""
+CREATE TABLE users ( user_id int, first_name varchar (255), last_name varchar (255), gender bool, level int);
 """)
 
 # song_id, title, artist_id, year, duration
 song_table_create = ("""
+CREATE TABLE songs (song_id varchar(255), title varchar (255), artist_id varchar (255), year int, duration int)
 """)
 
 # artist_id, name, location, latitude, longitude
 artist_table_create = ("""
+CREATE TABLE artists (artist_id varchar (255), name varchar(255), location varchar(255), latitude int null , longitude int null )
 """)
 
 # start_time, hour, day, week, month, year, weekday
 time_table_create = ("""
+CREATE TABLE times (hour int, day int, week int, month int, year int, weekday int)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
-
+INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
 """)
 
 
 time_table_insert = ("""
+INSERT INTO times (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
@@ -53,6 +62,5 @@ song_select = ("""
 """)
 
 # QUERY LISTS
-
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
